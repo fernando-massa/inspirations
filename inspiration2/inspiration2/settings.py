83cac21958c050c53dd5c83a8b660383a9026cd2
heyitsmebev <beverly.nguyen0907@gmail.com>
2022-09-20 16:34:05 -0700

    css static

--- inspiration2/inspiration2/settings.py
+++ inspiration2/inspiration2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 # Configure Django App for Heroku.
 import django_on_heroku
+import os
 
 from pathlib import Path
 import environ
@@ -142,6 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
