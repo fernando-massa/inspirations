69487823d2b4eed4e6bf967f248adff549384f8a
heyitsmebev <beverly.nguyen0907@gmail.com>
2022-09-20 14:57:31 -0700

    gallery CRUD

--- inspiration2/main_app/urls.py
+++ inspiration2/main_app/urls.py
@@ -27,6 +27,19 @@ urlpatterns = [
     path('inspirations/<int:pk>/delete/',
         views.InspirationDelete.as_view(), name='inspirations_delete'),
 
+# ------------------Gallery-------------------------------------------------
+    # http://localhost:8000/galleries/
+    path('galleries/', views.GalleryList.as_view(), name='galleries_index'),
+    # http://localhost:8000/galleries/1/
+    path('galleries/<int:pk>/', views.GalleryDetail.as_view(), name='galleries_detail'),
+    # http://localhost:8000/galleries/create/
+    path('galleries/create/', views.GalleryCreate.as_view(), name='galleries_create'),
+    # http://localhost:8000/galleries/1/update/
+    path('galleries/<int:pk>/update/', views.GalleryUpdate.as_view(), name='galleries_update'),
+    # http://localhost:8000/galleries/1/delete/
+    path('galleries/<int:pk>/delete/', views.GalleryDelete.as_view(), name='galleries_delete'),
+
+
 # ------------------Sign up-------------------------------------------------
 
     path('accounts/signup/', views.signup, name='signup'),
