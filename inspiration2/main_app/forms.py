8f2aa4301b737ace5b30a6e890e686131d2d3939
patricktien <patrick1996112@gmail.com>
2022-09-21 17:19:09 -0700

    update m2m inspirations to gallery

--- inspiration2/main_app/forms.py
+++ inspiration2/main_app/forms.py
@@ -1,6 +1,25 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Note
+from .models import Inspiration
+from .models import Gallery
 
+class CustomMMCF(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, gallery):
+        return "%s" % gallery.name
+
+
+class AddGalleriesForm(forms.ModelForm):
+    class Meta:
+        model = Inspiration
+        fields = ['name', 'description', 'link', 'galleries']
+        name = forms.CharField(max_length=500)
+        description = forms.CharField(max_length=1000)
+        link = forms.CharField(max_length=1000)
+        galleries = forms.ModelMultipleChoiceField(
+        queryset=Gallery.objects.values('name'),
+        widget=forms.CheckboxSelectMultiple
+        )
 
 class NoteForm(ModelForm):
     class Meta:
